653f9c9c11b67f389fa8fa0bc8bb885728143045
Derrick Narkah <33673941+derricknarkah@users.noreply.github.com>
2024-07-23 10:57:14 -0500

    Add files via upload

---
+++ SQL/6. Limit and Aliasing.sql
@@ -0,0 +1,13 @@
+-- Limit & Aliasing
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1
+;
+
+SELECT gender, AVG(age) AS avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40
+;
